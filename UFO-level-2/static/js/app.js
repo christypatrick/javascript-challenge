5753ee41f83c752873b3cd7dad00aa18d8ba7995
Christy Patrick <christy06p@msn.com>
2020-11-12 17:56:12 -0600

    updated level 2 with filtered data

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -7,8 +7,9 @@ var tbody = d3.select("tbody");
 // Console.log the UFO data from data.js
 console.log(data);
 
-// Step 1: Loop Through `data` and log each UFO report object
-data.forEach(ufo => {
+function defaultTable(data) {
+  tbody.html(""); 
+  data.forEach(ufo => {
     console.log(ufo);
   
     // Step 2:  Use d3 to append one table row `tr` for each UFO report object
@@ -26,128 +27,61 @@ data.forEach(ufo => {
       cell.text(value);
     });
   });
+}
+
+defaultTable(data);
 
 /////reset table/////
 
-// Get a reference to the form controls
-var formControl = d3.select(".form-control");
+// // Get a reference to the form controls
+var formControl = d3.selectAll(".form-control");
 
-// Select the button
+// // Select the button
 var resetButton = d3.select("#reset-btn");
 
 resetButton.on("click", function() {
-// remove any data from the table
-tbody.html("");
 
-// remove any data from the form
+// // remove any data from the form
 formControl.html("");
 
-// Step 1: Loop Through `data` and log each UFO report object
-data.forEach(ufo => {
-  console.log(ufo);
-
-  // Step 2:  Use d3 to append one table row `tr` for each UFO report object
-  var row = tbody.append("tr");
-
-  // Step 3:  Use `Object.entries` to log each UFO report value
-  Object.entries(ufo).forEach(([key, value]) => {
-    console.log(key, value);
-
-    // Step 4: Use d3 to append 1 cell per UFO report value 
-    var cell = row.append("td");
-
-    // Step 5: Use d3 to update each cell's text with
-    // ufo report values
-    cell.text(value);
-  });
-});
-});
-
-
-  ////////////////////// filtering by date //////////////////////
-
-// Assign the data from `data.js` to a descriptive variable
-var sightings = data;
-
-// Select the button
-var button = d3.selectAll("#filter-btn");
-
-button.on("click", function() {
-
-// Select the input element and get the raw HTML node
-var dateInputElement = d3.select("#datetime");
-var cityInputElement = d3.select("#city");
-var stateInputElement = d3.select("#state");
-var countryInputElement = d3.select("#country");
-var shapeInputElement = d3.select("#shape");
-
-// Get the value property of the input element
-var dateInputValue = dateInputElement.property("value");
-var cityInputValue = cityInputElement.property("value");
-var stateInputValue = stateInputElement.property("value");
-var countryInputValue = countryInputElement.property("value");
-var shapeInputValue = shapeInputElement.property("value");
-
-console.log(dateInputValue);
-console.log(cityInputValue);
-console.log(stateInputValue);
-console.log(countryInputValue);
-console.log(shapeInputValue);
-
-//console.log(sightings);
-
-
-
-
-// if(dateInputValue !== ""){var filteredData = sightings.filter(function(obj){ return obj["datetime"] == dateInputValue })};
-// if(cityInputValue !== ""){var filteredData = sightings.filter(function(obj){ return obj["city"] == cityInputValue })};
-// if(stateInputValue !== ""){var filteredData = sightings.filter(function(obj){ return obj["state"] == stateInputValue })};
-// if(countryInputValue !== ""){var filteredData = sightings.filter(function(obj){ return obj["country"] == countryInputValue })};
-// if(shapeInputValue !== ""){var filteredData = sightings.filter(function(obj){ return obj["shape"] == shapeInputValue })};
-
-// if(cityInputValue !== ""){var filteredData = filteredData.filter(sighting => sighting.city === cityInputValue)};
-// if(stateInputValue !== ""){var filteredData = filteredData.filter(sighting => sighting.state === stateInputValue)};
-// if(countryInputValue !== ""){var filteredData = filteredData.filter(sighting => sighting.country === countryInputValue)};
-// if(shapeInputValue !== ""){var filteredData = filteredData.filter(sighting => sighting.shape === shapeInputValue)};
-
-// var filteredData = sightings.filter(sighting => sighting.datetime === dateInputValue ||
-//                                                 sighting.city === cityInputValue ||
-//                                                 sighting.state === stateInputValue ||
-//                                                 sighting.country === countryInputValue ||
-//                                                 sighting.shape === shapeInputValue);
-
-var filteredData = sightings.filter(sighting => sighting.datetime === dateInputValue)
-                            .filter(sighting => sighting.city === cityInputValue)
-                            .filter(sighting => sighting.state === stateInputValue)
-                            .filter(sighting => sighting.country === countryInputValue)
-                            .filter(sighting => sighting.shape === shapeInputValue);
-
-console.log(filteredData);
-
-// Get a reference to the table body
-var tbody = d3.select("tbody");
-
-// remove any data from the table
+// remove any data from the tables
 tbody.html("");
 
-
-// Step 1: Loop Through `data` and log each UFO report object
-filteredData.forEach(ufoFiltered => {
-    console.log(ufoFiltered);
-  
-    // Step 2:  Use d3 to append one table row `tr` for each UFO report object
-    var row = tbody.append("tr");
-  
-    // Step 3:  Use `Object.entries` to log each UFO report value
-    Object.entries(ufoFiltered).forEach(([key, value]) => {
-      console.log(key, value);
-  
-      // Step 4: Use d3 to append 1 cell per UFO report value 
-      var cell = row.append("td");
-  
-      // Step 5: Use d3 to update each cell's text with
-      // ufo report values
-      cell.text(value);
-    });
-  });
+//load the default table
+defaultTable(data)
 });
+
+
+////////////////////// filtering data /////////////////////
+
+// Create a dictionary for the filters
+var filterDictionary = {};
+
+function updateFilters () {
+  var grabbedElement = d3.select(this).select("input");
+  var inputValue = grabbedElement.property("value")
+  var genericID = grabbedElement.attr("id");
+  // console.log(inputValue)
+  if (inputValue) {
+    filterDictionary[genericID] = inputValue;
+  } else {
+    delete filterDictionary[genericID]
+  }
+  filteredTable();
+}
+
+function filteredTable() {
+  let filteredData = tableData;
+
+  Object.entries(filterDictionary).forEach(([key, value]) => {
+    filteredData = filteredData.filter(row => row[key] === value);
+  });
+  defaultTable(filteredData);
+}
+
+
+d3.selectAll(".filter").on("change", updateFilters);
+
+var button = d3.selectAll("#filter-btn");
+
+
